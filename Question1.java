2715839e6b915c0a570010a96f10ebba535f815b
aa785214 <a830124q@gmail.com>
2015-04-27 10:58:25 +0800

    Update Question1.java
    A revert C

--- Question1.java
+++ Question1.java
@@ -1,7 +1,7 @@
 ﻿public class Question1{
 	public statis void main(String[] args){
 		txtCopy("Source.txt", "Dest.txt");
-		System.out.println("C");
+		System.out.println("Finish");
 		editFile("Dest.txt");
 		imgCopy("Source.jpg", "Dest.jpg");
 	}
