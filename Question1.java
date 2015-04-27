39a3920abb909bf9dba3be3d1bf42ac0e33c2b4e
aa785214 <a830124q@gmail.com>
2015-04-27 10:42:52 +0800

    Update Question1.java
    A AM011807 邱義瀚提交

--- Question1.java
+++ Question1.java
@@ -1,6 +1,7 @@
 ﻿public class Question1{
 	public statis void main(String[] args){
 		txtCopy("Source.txt", "Dest.txt");
+		System.out.println("A");
 		editFile("Dest.txt");
 		imgCopy("Source.jpg", "Dest.jpg");
 	}
