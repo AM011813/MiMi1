39d21778fe2463f77262a03045222351b4bc39b2
fred08314 <fred08314@hotmail.com.tw>
2015-04-27 10:45:47 +0800

    Update Question1.java
    B AM012265 陳亮瑋提交

--- Question1.java
+++ Question1.java
@@ -1,7 +1,7 @@
 ﻿public class Question1{
 	public statis void main(String[] args){
 		txtCopy("Source.txt", "Dest.txt");
-		System.out.println("A");
+		System.out.println("B");
 		editFile("Dest.txt");
 		imgCopy("Source.jpg", "Dest.jpg");
 	}
