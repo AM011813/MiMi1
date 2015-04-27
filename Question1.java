9c8aea31185492237744fcc93c1d7509f7394e9a
aa785214 <a830124q@gmail.com>
2015-04-27 10:26:48 +0800

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -1,6 +1,7 @@
 ﻿public class Question1{
 	public statis void main(String[] args){
 		txtCopy("Source.txt", "Dest.txt");
+		System.out.println(“A AM011807 邱義瀚提交”);
 		System.out.println(“B AM012265 陳亮偉提交”);
 		editFile("Dest.txt");
 		imgCopy("Source.jpg", "Dest.jpg");
